ef30bb16200fa6b9ea90dee378330436c3389de1
raythurman2386 <raymondthurman5@gmail.com>
2019-12-26 16:47:57 -0500

    initial routes all working

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,7 +3,24 @@ let Exercise = require('../models/exercise.model');
 
 router
   .get('/', (req, res) => {
-    return res.json("It's working")
+    Exercise.find()
+      .then(exercises => res.json(exercises))
+      .catch(err => res.status(400).json(err.response))
+  })
+
+  .post('/add', (req, res) => {
+    const user = {
+      username: req.body.username,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: Date.parse(req.body.date)
+    }
+
+    const newExercise = new Exercise(user)
+
+    newExercise.save()
+      .then(() => res.json('Exercise Added!'))
+      .catch(err => res.status(400).json(err.response))
   })
 
 module.exports = router;

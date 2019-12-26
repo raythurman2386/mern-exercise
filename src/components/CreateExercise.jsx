6f1a36aa125594c04986d1dd5d585aa2c106dcee
raythurman2386 <raymondthurman5@gmail.com>
2019-12-26 18:51:53 -0500

    create exercise, need refactor

--- src/components/CreateExercise.jsx
+++ src/components/CreateExercise.jsx
@@ -1,11 +1,118 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
-const CreateExercise = () => {
+const CreateExercise = (props) => {
+  const [username, setUsername] = useState('')
+  const [description, setDescription] = useState('')
+  const [duration, setDuration] = useState(0)
+  const [date, setDate] = useState(new Date())
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/users/')
+      .then(response => {
+        if (response.data.length > 0) {
+          setUsers(response.data.map(user => user.username))
+          setUsername(response.data[0].username)
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+
+  })
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  }
+
+  const onChangeDescription = (e) => {
+    setDescription(e.target.value)
+  }
+
+  const onChangeDuration = (e) => {
+    setDuration(e.target.value)
+  }
+
+  const onChangeDate = (date) => {
+    setDate(date)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const exercise = {
+      username,
+      description,
+      duration,
+      date
+    }
+
+    console.log(exercise);
+
+    axios.post('http://localhost:4000/exercises/add', exercise)
+      .then(res => console.log(res.data));
+
+    props.history.push('/')
+  }
+
   return (
     <div>
-      <h2>Create Ex</h2>
+      <h3>Create New Exercise Log</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <select
+            required
+            className="form-control"
+            value={username}
+            onChange={e => onChangeUsername(e)}>
+            {
+              users.map((user) => {
+                return <option
+                  key={user}
+                  value={user}>{user}
+                </option>;
+              })
+            }
+          </select>
+        </div>
+        <div className="form-group">
+          <label>Description: </label>
+          <input type="text"
+            required
+            className="form-control"
+            value={description}
+            onChange={e => onChangeDescription(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duration (in minutes): </label>
+          <input
+            type="text"
+            className="form-control"
+            value={duration}
+            onChange={e => onChangeDuration(e)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Date: </label>
+          <div>
+            <DatePicker
+              selected={date}
+              onChange={onChangeDate}
+            />
+          </div>
+        </div>
+
+        <div className="form-group">
+          <input type="submit" value="Create Exercise Log" className="btn btn-primary" />
+        </div>
+      </form>
     </div>
   )
 }
 
-export default CreateExercise
+export default CreateExercise;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
-render(<Router><App /></Router>, document.getElementById('root'));
+const AppWithRouter = withRouter(App)
+
+render(<Router><AppWithRouter /></Router>, document.getElementById('root'));

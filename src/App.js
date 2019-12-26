7fa1d456ea8fb1922ebcf8e3c0c8078fe21f7934
raythurman2386 <raymondthurman5@gmail.com>
2019-12-26 17:23:41 -0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+// Components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Testing</h1>
     </div>
   );

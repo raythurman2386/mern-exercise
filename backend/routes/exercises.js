7342b616b567212d5780076a6a446e99c6e09620
raythurman2386 <raymondthurman5@gmail.com>
2019-12-26 17:02:25 -0500

    full crud for exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -8,6 +8,18 @@ router
       .catch(err => res.status(400).json(err.response))
   })
 
+  .get('/:id', (req, res) => {
+    Exercise.findById(req.params.id)
+      .then(exercise => res.json(exercise))
+      .catch(err => res.status(400).json(err.response))
+  })
+
+  .delete('/:id', (req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Exercise Deleted!'))
+      .catch(err => res.status(400).json(err.response))
+  })
+
   .post('/add', (req, res) => {
     const user = {
       username: req.body.username,
@@ -20,6 +32,21 @@ router
 
     newExercise.save()
       .then(() => res.json('Exercise Added!'))
+      .catch(err => res.status(400).json(err.response))
+  })
+
+  .put('/update/:id', (req, res) => {
+    Exercise.findById(req.params.id)
+      .then(exercise => {
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = req.body.duration;
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+          .then(() => res.json('Exercise Updated!'))
+          .catch(err => res.status(400).json(err.response))
+      })
       .catch(err => res.status(400).json(err.response))
   })
 

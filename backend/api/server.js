595ba4b01aa9f84b9a46971e246d9ee5f31c2c07
raythurman2386 <raymondthurman5@gmail.com>
2019-12-27 22:38:37 -0500

    refactored middleware slightly

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
-const cors = require("cors");
+const middleware = require('./middleware');
 const routes = require('./routes');
 
 const server = express();
 
-server.use(cors());
-server.use(express.json());
+middleware(server);
 routes(server);
 
 module.exports = server;

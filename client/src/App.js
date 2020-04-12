dafbb967c15b163ca00d2de5cd1a66f2358bd088
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-12 00:55:09 -0400

    Feat: moved navbar into layout component

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 // Components
-import Navbar from './components/Navbar/Navbar';
+import Layout from './components/Layout/Layout';
 import ExerciseList from './components/Exercise/ExerciseList';
 import EditExercise from './components/Exercise/EditExercise';
 import CreateExercise from './components/Exercise/CreateExercise';
 
 function App() {
 	return (
-		<>
-			<Navbar />
-			<br />
+		<Layout>
 			<Route exact path='/' component={ExerciseList} />
 			<Route path='/edit/:id' component={EditExercise} />
 			<Route path='/create' component={CreateExercise} />
-		</>
+		</Layout>
 	);
 }
 

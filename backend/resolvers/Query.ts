7e49bab9169775d70ed6592140a91180c5cd4c17
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-11 21:16:15 -0400

    Feat: basic queries set up

--- backend/resolvers/Query.ts
+++ backend/resolvers/Query.ts
@@ -1,4 +1,5 @@
 import User from '../models/user.model';
+import Exercise from '../models/exercise.model';
 
 const hello = () => {
   return 'Hello world';
@@ -8,4 +9,16 @@ const users = () => {
   return User.find();
 };
 
-export default { hello, users };
+const user = (_parent: any, args: { id: number }) => {
+  return User.findById(args.id);
+};
+
+const exercises = () => {
+  return Exercise.find();
+};
+
+const exercise = (_parent: any, args: { id: number }) => {
+  return Exercise.findById(args.id);
+};
+
+export default { hello, users, user, exercises, exercise };
--- backend/typeDefs/index.ts
+++ backend/typeDefs/index.ts
@@ -4,12 +4,23 @@ const typeDefs = gql`
   type Query {
     hello: String
     users: [User]!
+    user(id: ID!): User
+    exercises: [Exercise]!
+    exercise(id: ID!): Exercise
   }
 
   type User {
     username: String!
     email: String!
     password: String!
+  }
+
+  type Exercise {
+    username: String!
+    description: String!
+    reps: Int!
+    sets: Int!
+    date: String!
   }
 `;
 

dd7be5bd6cac222d4e22e768d1f48da66ffbb1b8
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-11 23:39:09 -0400

    Fix: added welcome route

--- backend/api/server.ts
+++ backend/api/server.ts
@@ -12,6 +12,10 @@ const apolloServer = new ApolloServer({
   playground: process.env.NODE_ENV !== 'production',
 });
 
+app.get('/', (_req, res) => {
+  res.json({ message: 'Welcome to your Exercise Tracker API' });
+});
+
 apolloServer.applyMiddleware({ app, cors: { origin: '*', credentials: true } });
 
 export default app;

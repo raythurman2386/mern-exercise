23e7948e7714705c7d907deada918b91007c034d
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-11 20:49:37 -0400

    Feat: Refactored initial models

---
+++ backend/models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 8,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;

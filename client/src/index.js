dfd232b10231d01f5f7675367aab50e8b3a5b4c6
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-12 01:28:20 -0400

    Feat: Added tailwindcss

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,8 @@ import { render } from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter as Router, withRouter } from 'react-router-dom';
-import './index.css';
+import './assets/main.css';
 import App from './App';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const client = new ApolloClient({
 	uri: process.env.BACKEND_URL || 'http://localhost:8000/graphql',

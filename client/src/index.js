908d7a2b89bcd57ef27ae459fed33c909faf788c
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-12 11:24:13 -0400

    Feat: minor backend refactor. Added inmemory cache

--- client/src/index.js
+++ client/src/index.js
@@ -2,22 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import './assets/main.css';
 import App from './App';
 
-const client = new ApolloClient({
-	uri:
-		'https://lift-track-mern.herokuapp.com/graphql' ||
-		'http://localhost:8000/graphql',
-});
+const cache = new InMemoryCache();
 
-const AppWithRouter = withRouter(App);
+const client = new ApolloClient({
+	uri: process.env.BACKEND_URL || 'http://localhost:8000/graphql',
+	cache,
+});
 
 render(
 	<ApolloProvider client={client}>
 		<Router>
-			<AppWithRouter />
+			<App />
 		</Router>
 	</ApolloProvider>,
 	document.getElementById('root')

6fba1704b07c7c4ac0ed3325648f85b2a8024df9
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-15 17:03:20 -0400

    Removed excess boilerplate

--- client/src/index.js
+++ client/src/index.js
@@ -4,15 +4,15 @@ import { render } from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import { InMemoryCache } from 'apollo-cache-inmemory';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import './assets/main.css';
 import App from './App';
 
-// const cache = new InMemoryCache();
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
 	uri: process.env.BACKEND_URL || 'http://localhost:8000/graphql',
-	// cache,
+	cache,
 });
 
 render(

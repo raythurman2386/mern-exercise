139f902416a1a056809b04b3d899c8da59b8c4dc
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-24 21:08:43 -0400

    Responsive nav implemented

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,41 +5,63 @@ const Navbar = () => {
 	const loggedIn = localStorage.getItem('token');
 
 	return (
-		<nav className='bg-gray-800 text-gray-300 flex py-4 px-2 justify-between fixed top-0 w-full'>
-			<Link to='/' className='cursor-pointer'>
-				Lift Tracker
-			</Link>
-			<div className='invisible md:visible'>
-				<ul className='flex'>
+		<nav className='flex items-center justify-between flex-wrap bg-gray-800 p-6'>
+			<div className='flex items-center flex-shrink-0 text-white mr-6'>
+				<Link to='/'>
+					<span className='font-semibold text-xl tracking-tight'>
+						Lift Tracker
+					</span>
+				</Link>
+			</div>
+			<div className='block md:hidden'>
+				<button className='flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white'>
+					<svg
+						className='fill-current h-3 w-3'
+						viewBox='0 0 20 20'
+						xmlns='http://www.w3.org/2000/svg'
+					>
+						<title>Menu</title>
+						<path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+					</svg>
+				</button>
+			</div>
+			<div className='w-full block flex-grow md:flex md:items-center md:w-auto'>
+				<div className='text-sm md:flex-grow'>
 					{loggedIn && (
-						<li className='cursor-pointer'>
-							<Link to='/exercises' className='nav-link'>
-								Exercises
-							</Link>
-						</li>
+						<Link
+							to='/exercises'
+							className='block mt-4 md:inline-block md:mt-0 text-teal-200 hover:text-white mr-4'
+						>
+							Exercises
+						</Link>
 					)}
 					{!loggedIn && (
-						<li className='ml-2 cursor-pointer'>
-							<Link to='/signup' className='nav-link'>
-								Sign Up
-							</Link>
-						</li>
+						<Link
+							to='/login'
+							className='block mt-4 md:inline-block md:mt-0 text-teal-200 hover:text-white mr-4'
+						>
+							Login
+						</Link>
 					)}
 					{!loggedIn && (
-						<li className='ml-2 cursor-pointer'>
-							<Link to='/login' className='nav-link'>
-								Login
-							</Link>
-						</li>
+						<Link
+							to='/signup'
+							className='block mt-4 md:inline-block md:mt-0 text-teal-200 hover:text-white'
+						>
+							Sign Up
+						</Link>
 					)}
-					{loggedIn && (
-						<li className='ml-2 cursor-pointer'>
-							<Link to='/' className='nav-link'>
-								Logout
-							</Link>
-						</li>
-					)}
-				</ul>
+				</div>
+				{loggedIn && (
+					<div>
+						<Link
+							to='/'
+							className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0'
+						>
+							Logout
+						</Link>
+					</div>
+				)}
 			</div>
 		</nav>
 	);

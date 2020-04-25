033699d5eb736d0b4cd0a613f127f85a5761335c
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-24 21:45:30 -0400

    Fixed logged in nav and sidebar styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
 					<div>
 						<Link
 							to='/'
-							className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0'
+							className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 md:mt-0'
 						>
 							Logout
 						</Link>
--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
 	return (
-		<div className='pt-4 w-64 h-screen bg-gray-900 overflow-hidden hidden md:flex flex-shrink-0 justify-center'>
+		<div className='pt-6 w-64 h-screen bg-gray-900 overflow-hidden hidden md:flex md:flex-col flex-shrink-0'>
 			{localStorage.getItem('token') ? (
 				<>
-					<h1 className='mt-6 pb-2 text-center text-gray-200 '>
-						Welcome Back User
-					</h1>
+					<h1 className='pb-2 text-center text-gray-200 '>Welcome Back User</h1>
 
-					<ul className='text-center text-gray-400 justify-center'>
-						<li>
+					<ul className='w-full text-center text-gray-400 justify-center'>
+						<li className='mb-2 py-2 w-full hover:bg-gray-800 cursor-pointer'>
 							<Link to='/exercise/create'>Add Exercise</Link>
 						</li>
-						<li>View Profile</li>
+						<li className='mb-2 py-2 w-full hover:bg-gray-800 cursor-pointer'>
+							View Profile
+						</li>
 					</ul>
 				</>
 			) : null}

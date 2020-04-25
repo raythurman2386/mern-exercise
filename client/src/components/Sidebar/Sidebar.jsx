1d5d6afcedac652274b4621b4829107d6db80ddd
Raymond Thurman <raymondthurman5@gmail.com>
2020-04-24 21:16:42 -0400

    Minor sidebar styles

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
 	return (
-		<div className='w-1/6 h-screen bg-gray-900 overflow-hidden'>
+		<div className='pt-4 w-64 h-screen bg-gray-900 overflow-hidden hidden md:flex flex-shrink-0 justify-center'>
 			{localStorage.getItem('token') ? (
 				<>
 					<h1 className='mt-6 pb-2 text-center text-gray-200 '>
 						Welcome Back User
 					</h1>
 
-					<ul className='text-center text-gray-400'>
+					<ul className='text-center text-gray-400 justify-center'>
 						<li>
 							<Link to='/exercise/create'>Add Exercise</Link>
 						</li>
@@ -20,11 +20,19 @@ const Sidebar = () => {
 			) : null}
 
 			<br />
-			<ul className='text-center text-gray-400'>
-				<li>Workouts</li>
-				<li>Programs</li>
-				<li>Supplements</li>
-				<li>Gym Wear</li>
+			<ul className='w-full text-center text-gray-400'>
+				<li className='mb-2 py-2 w-full hover:bg-gray-800 cursor-pointer'>
+					Workouts
+				</li>
+				<li className='mb-2 py-2 w-full hover:bg-gray-800 cursor-pointer'>
+					Programs
+				</li>
+				<li className='mb-2 py-2 w-full hover:bg-gray-800 cursor-pointer'>
+					Supplements
+				</li>
+				<li className='mb-2 py-2 w-full hover:bg-gray-800 cursor-pointer'>
+					Gym Wear
+				</li>
 			</ul>
 		</div>
 	);

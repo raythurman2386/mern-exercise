bd55d9326d3cb28e0630861b2398f7312a483a86
Raymond Thurman <raymondthurman5@gmail.com>
2020-05-17 20:18:17 -0400

    refactored to use new models

--- backend/src/resolvers/User.ts
+++ backend/src/resolvers/User.ts
@@ -1,7 +1,7 @@
-import Exercise from '../models/exercise.model';
+import { ExerciseModel } from '../database/schema';
 
 const exercises = async (parent: { username: string }, _args: any) => {
-  const exercise = await Exercise.find({ username: parent.username });
+  const exercise = await ExerciseModel.find({ username: parent.username });
 
   return exercise;
 };
